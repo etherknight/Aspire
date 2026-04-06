666f207233b38f5dd155237c41f65fb52ce0aaf8
= <=>
2026-04-06 10:30:06 +0100

    Added mapper for minimal api routes

---
+++ Project.Api/Controllers/ControllerModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace Project.Api.Controllers;
+
+public interface IApiRouteBuilder {
+    void Map(IEndpointRouteBuilder app);
+}
+
+public static class ControllerModule {
+    extension(IEndpointRouteBuilder app) {
+        public void MapProjectApi() {
+            IEnumerable<Type> controllers = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(type => typeof(IApiRouteBuilder).IsAssignableFrom(type));
+            
+            foreach (Type type in controllers) {
+                var controller = Activator.CreateInstance(type);
+                if (controller is IApiRouteBuilder route) {
+                    route.Map(app);
+                }
+            }
+        }
+    }
+}
+
