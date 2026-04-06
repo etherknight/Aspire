1cfa36b804a73f43bd7300bdbab44514d891d5cb
= <=>
2026-04-06 10:28:07 +0100

    Moved SystemController to minimal api

--- Project.Api/Controllers/SystemController.cs
+++ Project.Api/Controllers/SystemController.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 using Project.BusinessLogic.Core;
@@ -8,25 +9,13 @@ namespace Project.Api.Controllers;
 /// <summary>
 /// Invoke system commands.
 /// </summary>
-[Route("[controller]")]
-[ApiController]
-public class SystemController : ControllerBase
-{
-    private readonly ISender _sender;
-
-    public SystemController(ISender sender)
-    {
-        _sender = sender;
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
-    [HttpGet("startup")]
-    public async Task<IActionResult> Startup(CancellationToken token)
-    {
-        await _sender.Send(new DatabaseInitCommand(), token);
-        return Ok();
+public class SystemController : IApiRouteBuilder{
+    const string apiName = "system";
+    
+    public void Map(IEndpointRouteBuilder app) {
+        app.MapGet("${apiName}/startup", async ([FromServices] ISender sender, CancellationToken token) => {
+            await sender.Send(new DatabaseInitCommand(), token);
+            return Results.Ok();
+        });
     }
 }
